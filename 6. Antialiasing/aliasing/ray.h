9fd76a37f33a60899198b2d59b344af3352ed8a6
atanteck <atanteck@caltech.edu>
2021-09-05 15:08:39 -0700

    Added in aliasing by increased sampling.

---
+++ 6. Antialiasing/aliasing/ray.h
@@ -0,0 +1,20 @@
+#ifndef RAY_H
+#define RAY_H
+
+#include "vec3.h"
+
+class ray {
+public:
+	point3 orig;
+	vec3 dir;
+	ray() {}
+	ray(point3 origin, vec3 direction) : orig(origin), dir(direction) {}
+
+	point3 origin() const { return orig; }
+	vec3 direction() const { return dir; }
+
+	point3 at(const double t) const {
+		return orig + t * dir;
+	}
+};
+#endif
